3cf6e9ff2508c1a76266932127dff2ec55aee9d9
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-08-20 17:56:13 +0800

    Implement Level 2

--- src/main/java/Barcelona.java
+++ src/main/java/Barcelona.java
@@ -3,25 +3,36 @@ import java.util.*;
 public class Barcelona {
     public static void main(String[] args) {
         String LINE = "____________________________________________________________";
-    String greet = LINE + """
-            \nHello! I'm Barcelona
-            What can I do for you?\n"""
-            + LINE;
-    Scanner sc = new Scanner(System.in);
+        String[] arr = new String[100];
+        int elems = 0;
 
-    String exit = LINE + """
-            \nBye. Hope to see you again soon!
-            """ + LINE;
+        String greet = LINE + """
+                \nHello! I'm Barcelona
+                What can I do for you?\n"""
+                + LINE;
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println(greet);
-    while (true) {
-        String input = sc.nextLine();
-        if (input.equals("bye")) {
-            break;
-        } else {
-            System.out.println(LINE + "\n" + input + "\n" +LINE);
+        String exit = LINE + """
+                \nBye. Hope to see you again soon!
+                """ + LINE;
+
+        System.out.println(greet);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                StringBuilder list = new StringBuilder();
+                for (int j=1; j<=elems; j++) {
+                    list.append(j).append(". ").append(arr[j - 1]).append("\n");
+                }
+                System.out.println(LINE + "\n" + list.toString() + LINE);
+            }
+            else {
+                arr[elems++] = input;
+                System.out.println(LINE + "\n added: " + input + "\n" + LINE);
+            }
         }
-    }
-    System.out.println(exit);
+        System.out.println(exit);
     }
 }
