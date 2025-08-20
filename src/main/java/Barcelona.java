c43303044855d1aeda9d2fe8b74b4c13dc8bf9e3
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-08-20 17:41:44 +0800

    Implement Level 1

--- src/main/java/Barcelona.java
+++ src/main/java/Barcelona.java
@@ -1,17 +1,27 @@
+import java.util.*;
+
 public class Barcelona {
     public static void main(String[] args) {
-    String greet = """
-            ____________________________________________________________
-            Hello! I'm Barcelona
-            What can I do for you?
-            ____________________________________________________________""";
+        String LINE = "____________________________________________________________";
+    String greet = LINE + """
+            \nHello! I'm Barcelona
+            What can I do for you?\n"""
+            + LINE;
+    Scanner sc = new Scanner(System.in);
 
-    String exit = """
-            Bye. Hope to see you again soon!
-            ____________________________________________________________
-            """;
+    String exit = LINE + """
+            \nBye. Hope to see you again soon!
+            """ + LINE;
 
     System.out.println(greet);
+    while (true) {
+        String input = sc.nextLine();
+        if (input.equals("bye")) {
+            break;
+        } else {
+            System.out.println(LINE + "\n" + input + "\n" +LINE);
+        }
+    }
     System.out.println(exit);
     }
 }
