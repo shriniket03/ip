5c0861466c258104e079690f7cc3b74f82a0e017
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-08-20 17:14:23 +0800

    Implement Level 0

---
+++ src/main/java/Barcelona.java
@@ -0,0 +1,17 @@
+public class Barcelona {
+    public static void main(String[] args) {
+    String greet = """
+            ____________________________________________________________
+            Hello! I'm Barcelona
+            What can I do for you?
+            ____________________________________________________________""";
+
+    String exit = """
+            Bye. Hope to see you again soon!
+            ____________________________________________________________
+            """;
+
+    System.out.println(greet);
+    System.out.println(exit);
+    }
+}
