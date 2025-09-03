8668a16094ebc52bc07d92b58c9bd91ef256593c
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-09-03 22:31:42 +0800

    Fix checkstyle

--- src/main/java/barcelona/ui/Ui.java
+++ src/main/java/barcelona/ui/Ui.java
@@ -2,17 +2,30 @@ package barcelona.ui;
 
 public class Ui {
     private final String line = "____________________________________________________________";
+
+    /**
+     * To greet user in the non-GUI format
+     */
     public void greet() {
         System.out.println(line + """
                 \nHello! I'm Barcelona
                 What can I do for you?\n"""
                 + line);
     }
+
+    /**
+     * To exit application in the non-GUI format
+     */
     public void exit() {
         System.out.println(line + """
                 \nBye. Hope to see you again soon!
                 """ + line);
     }
+
+    /**
+     * To log errors in the non-GUI format
+     * @param message - message to be logged
+     */
     public void log(String message) {
         System.out.println(line + "\n" + message + "\n" + line);
     }
