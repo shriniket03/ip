8b57eda534263bbad7e121c0bb05c37f7b8e6cfc
Shriniket <24452933+shriniket03@users.noreply.github.com>
2025-09-05 22:07:31 +0800

    Enhance javadoc for Todos class

--- src/main/java/barcelona/task/Todos.java
+++ src/main/java/barcelona/task/Todos.java
@@ -1,7 +1,12 @@
 package barcelona.task;
 
 /**
- * Creates a todos object with only description
+ * Represents a simple to-do task.
+ * <p>
+ * The {@code Todos} class extends {@link Task} without introducing
+ * additional properties. It is used for tasks that only require
+ * a description, without deadlines or event dates.
+ * </p>
  */
 public class Todos extends Task {
 
